392c0532d3a406d8e3c3194ec3ecba6896a19694
fpfrcarreiro <fpfr.carreiro@hotmail.com>
2015-12-13 18:32:50 +0000

    Criação test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Test::Application.config.secret_token = '27e34c8ddc45520d489f643bfebdb430e17d91d6bddde7e9e1b27377c316a665c82da01d60baf3b2388dfb6a5ce18ecefbcb3e43adba64abbe111903061900e7'
